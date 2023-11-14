1854cf67e4720785b085c39973f44f9d78f8d5c7
Madhu Duggirala <madhuduggirala@Madhus-MBP.lan>
2023-11-14 16:47:16 -0500

    added

--- regresTestCases.java
+++ regresTestCases.java
@@ -2,12 +2,10 @@ package day1;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
-
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
@@ -15,37 +13,38 @@ public class regresTestCases {
 	
 	@Test
 	public void testcase() {
-		// .body("data[0].email", equalTo("michael.lawson@reqres.in"));
-	//	https://reqres.in/api/users?page=2
+		// "michael.lawson@reqres.in";
+	   // https://reqres.in/api/users?page=2
 		
 		// part 1
+		
 		RestAssured.baseURI = "https://reqres.in/api";
 		given()
 		.when()
-		.get("/users?page=2")
+		.get("/users/2")
 		.then()
 		.statusCode(200)
-		//.log().all()
 		.header("Content-Type","application/json; charset=utf-8")
-		.body("data[0].email",equalTo("michael.lawson@reqres.in"));
+		.body("data.email",equalTo("janet.weaver@reqres.in"));
 		
 		// part 2
 	
 		RestAssured.baseURI = "https://reqres.in/api";
 		Response res = given()
-				.when()
-				.get("/users?page=2");
+				      .when()
+				      .get("/users/2");
 		Assert.assertEquals(res.getStatusCode(), 200);
 		Assert.assertEquals(res.header("Content-Type"),"application/json; charset=utf-8" );
 		
-		String email = res.jsonPath().get("data[0].email").toString();
-		Assert.assertEquals(email, "michael.lawson@reqres.in");	
+		String email = res.jsonPath().get("data.email").toString();
+		Assert.assertEquals(email, "janet.weaver@reqres.in");	
 	}
 	
 	@Test
 	public void testCase2() {
 		
 		RestAssured.baseURI = "https://reqres.in/api";
+		
 		boolean found = false;
 		Response res = RestAssured.given().when().get("/users?page=2");
 		
@@ -68,10 +67,29 @@ public class regresTestCases {
 		}
 		Assert.assertTrue(found);
 				
+	}	
+	
+	@Test
+	public void testCase3() {
+		RestAssured.baseURI = "https://reqres.in/api";
+		
+		boolean found = false;
+		Response res = given().when().get("users?page=2");
+		JSONObject jo = new JSONObject(res.asString());
+		
+		for(int i=0; i< jo.getJSONArray("data").length(); i++) {
+			
+			String firstName = jo.getJSONArray("data").getJSONObject(i).get("first_name").toString();
+			System.out.println(firstName);
+			
+			if(firstName.equals("George")) {
+				
+				found = true;
+				break;
+			}
+		}
+		Assert.assertTrue(found);
 	}
-	
-	
-	
 	
 	
 	
